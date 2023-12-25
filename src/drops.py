675697fd16563a39c43c4bdc861b090528ce87cf
pandabear189 <elin8340@gmail.com>
2023-12-24 21:52:03 -0600

    organization, subclassing, better demo/declutter of drops.ipynb

--- src/drops.py
+++ src/drops.py
@@ -2,126 +2,103 @@ from __future__ import annotations
 
 from typing import Literal, Callable
 
-import yaml
 import numpy as np
+import matplotlib.pyplot as plt
 
 import utils
+from utils.results import Results
 
 
-class Drops:
+class Drops(Results):
     def __init__(
             self,
             file_path: str,
             method: Literal["std_deviation", "mean", "iqr"],
             alpha: float = None
     ):
-        self.method: Callable = {"std_deviation": self.std_deviation, "mean": self.mean, "iqr": self.iqr}[method]
+        super().__init__(file_path)
+        self._method: Callable = {"std_deviation": self.std_deviation, "mean": self.mean, "iqr": self.iqr}[method]
         self.alpha = alpha or 2
-        self.data: dict = {}
-        self.team_list: list[dict[str, str | int]] = []
-        self.teams: dict[int, str] = {}
-        self.full_scores: dict[int, list[int]] = {}
-        self.events: list[str] = []
-        self.trial_events: list[str] = []
-        self.total_scores: dict[str, int] = {}
-        self.averages: dict[int, float] = {}
-        self.bombed_events: dict[int, list[str]] = {}
+        self.dropped_scores: dict[int, int] = {}
+        self.bombed_events: dict[int, list[str]]
+        self.averaged_bombed_events = sum([len(self.bombed_events[t]) for t in self.bombed_events]) / len(self.teams)
 
-        self.path: str = file_path
-
-        self.setup()
-
-    def setup(self):
-        with open(self.path, 'r') as file:
-            self.data = yaml.safe_load(file)
-
-        self.team_list = sorted(self.data.get("Teams"), key=lambda x: x['number'])
-        self.teams = {team['number']: (team['school'] + " " + team.get('suffix', "")) for team in
-                      self.team_list}  # team number : team name
-        self.events = [event['name'] for event in self.data.get('Events')]
-        self.trial_events = [d['name'] for d in self.data.get('Events') if d.get('trial', False)]
-        self.total_scores = {t: 0 for t in self.teams}  # team number : total score (sum)
-        self.full_scores = {t: [] for t in self.teams}  # full array of scores for each team
-
-        for placement in self.data.get('Placings'):
-            self.total_scores[placement['team']] += placement.get(
-                'place', len(self.teams)
-            ) if not placement['event'] in self.trial_events else 0
-
-            self.full_scores[placement['team']].append(
-                placement.get('place', len(self.teams))
-            ) if not placement['event'] in self.trial_events else 0
-
-        self.averages = {
-            t: self.total_scores[t] / (len(self.data.get('Events')) - len(self.trial_events)) for t in self.teams
-        }
+    def _populate(self) -> None:
+        super()._populate()
         self.bombed_events = {t: [] for t in self.teams}
 
     def iqr(self):
-        for placement in self.data.get('Placings'):
+        for placement in self.raw_placements:
             q1 = np.quantile(self.full_scores[placement['team']], 0.25)
             q3 = np.quantile(self.full_scores[placement['team']], 0.75)
             if (placement.get('place', len(self.teams)) > ((q3 - q1) * self.alpha + q3)
                     and not placement['event'] in self.trial_events):
                 self.bombed_events[placement['team']].append(placement['event'])
 
-        print("\"Bombed\" events by school:", utils.pretty_print(self.teams, self.bombed_events), "\n")
-        print(
-            "Average \"bombed events\": ",
-            sum([len(self.bombed_events[t]) for t in self.bombed_events]) / len(self.teams)
-        )
-
     def mean(self):
-        for placement in self.data.get('Placings'):
+        for placement in self.raw_placements:
             if (placement.get('place', len(self.teams)) > self.averages[placement['team']] * self.alpha
                     and not placement['event'] in self.trial_events):
                 self.bombed_events[placement['team']].append(placement['event'])
 
-        print("\"Bombed\" events by school:", utils.pretty_print(self.teams, self.bombed_events), "\n")
-        print(
-            "Average \"bombed events\": ",
-            sum([len(self.bombed_events[t]) for t in self.bombed_events]) / len(self.teams)
-        )
-
-    def std_deviation(self):
-        for placement in self.data.get('Placings'):
+    def std_deviation(self) -> None:
+        for placement in self.raw_placements:
             mean = np.mean(self.full_scores[placement['team']])
             std = np.std(self.full_scores[placement['team']])
             if (placement.get('place', len(self.teams)) > (mean + self.alpha * std)
                     and not placement['event'] in self.trial_events):
                 self.bombed_events[placement['team']].append(placement['event'])
 
-        print("\"Bombed\" events by school:", utils.pretty_print(self.teams, self.bombed_events), "\n")
-        print(
-            "Average \"bombed events\": ",
-            sum([len(self.bombed_events[t]) for t in self.bombed_events]) / len(self.teams)
-        )
-
-    def drop(self) -> dict[str, int]:
+    def drop(self) -> None:
         """
 
         :return:  dict[str, int], sorted scores {name: sum(event placements), ...} after dropping
         """
-        self.method()
-        score_copy = self.full_scores.copy()
-        drops = round(sum([len(self.bombed_events[t]) for t in self.bombed_events]) / len(self.teams))
+        self._method()
+        score_copy = self.full_scores
+        drop = round(sum([len(self.bombed_events[t]) for t in self.bombed_events]) / len(self.teams))
 
-        for _ in range(drops):
+        for _ in range(drop):
             for team in score_copy:
                 score_copy[team].remove(max(score_copy[team]))
 
         score_with_drops = {t: sum(score_copy[t]) for t in score_copy}
 
         sorted_scores = dict(sorted(score_with_drops.items(), key=lambda item: item[1]))
-        print(f"After dropping {drops} events: \n")
-        print(utils.pretty_print(self.teams, sorted_scores))
+        # print(f"After dropping {drop} events: \n")
+        # print(utils.pretty_print(self.teams, sorted_scores))
+        self.dropped_scores = sorted_scores
+        # return sorted_scores
 
-        return sorted_scores
+    def visualize(self):
+        fig, ax = plt.subplots()
+        ax.set_title("Distribution of scores")
+        ax.set_xlabel("Team Placement")
+        ax.set_ylabel("Score")
+
+        non_drop_rankings = sorted(self.score_sum, key=self.score_sum.get)
+        drop_rankings = sorted(self.dropped_scores, key=self.dropped_scores.get)
+
+        colors = {k: (np.random.random(), np.random.random(), np.random.random()) for k in self.teams}
+        ndrop_data = [(non_drop_rankings.index(i) + 1, self.score_sum[i], colors[i]) for i in self.teams]
+        drop_data = [(drop_rankings.index(i) + 1, self.dropped_scores[i], colors[i]) for i in self.teams]
+
+        ndrop_x, ndrop_y, ndrop_color = zip(*ndrop_data)
+        drop_x, drop_y, drop_color = zip(*drop_data)
+
+        ax.scatter(x=ndrop_x, y=ndrop_y, color=ndrop_color, label="Before dropping", marker="o")
+        ax.scatter(x=drop_x, y=drop_y, color=drop_color, label="After dropping", marker="*")
+
+        ax.set(xlim=(0, 61), xticks=np.arange(start=0, stop=61, step=5),
+               ylim=(0, 1000), yticks=np.arange(start=0, stop=max(self.score_sum.values()) + 200, step=100))
+        plt.legend(loc="upper left")
+        plt.show()
 
 
 if __name__ == '__main__':
     """
     Example usage:
     """
-    drops = Drops("../data/2023-05-20_nationals_c.yaml", "std_deviation", alpha=1.25)
+    drops = Drops("../data/2023-02-18_penn_invitational_c.yaml", "std_deviation", alpha=1.25)
     drops.drop()
+    drops.visualize()
---
+++ utils/results.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+import yaml
+
+
+class Results:
+    def __init__(self, results_path) -> None:
+        self.results_path = results_path
+        self._data: dict = {}
+        self._populate()
+        self._teams_data: list[dict[str, str | int]]
+        self._teams: dict[int, str]
+        self._events: list[str]
+        self._trial_events: list[str]
+        self._full_scores: dict[int, list[int]]
+        self._score_sum: dict[int, int]
+        self._averages: dict[int, float]
+        self._raw_placements: list[dict[str, str | int]]
+
+    def _populate(self) -> None:
+        self._load_data()
+        self._teams_data = sorted(self._data.get("Teams"), key=lambda x: x['number'])
+        self._teams = {team['number']: (team['school'] + " " + team.get('suffix', "")) for team in self._teams_data}
+        self._events = [event['name'] for event in self._data.get('Events')]
+        self._trial_events = [event['name'] for event in self._data.get('Events') if event.get('trial', False)]
+        full_scores = {t: [] for t in self.teams}
+        for placement in self._data.get('Placings'):
+            team_number = placement['team']
+            full_scores[team_number].append(
+                placement.get('place', len(self.teams))
+            ) if placement['event'] not in self.trial_events else 0
+        self._full_scores = full_scores
+        self._score_sum = {t: sum(self.full_scores[t]) for t in self.teams}
+        self._averages = {t: self.score_sum[t] / (len(self.events) - len(self.trial_events)) for t in self.teams}
+        self._raw_placements = self._data.get('Placings')
+
+    def _load_data(self) -> None:
+        with open(self.results_path, 'r') as file:
+            self._data = yaml.safe_load(file)
+
+    @property
+    def teams_data(self) -> list[dict[str, str | int]]:
+        """
+        :return: List of dictionaries containing team data
+        :exe: [{'number': 1, 'school': 'School Name', 'suffix': 'A'}, ...]
+        """
+
+        return self._teams_data
+
+    @property
+    def teams(self) -> dict[int, str]:
+        """
+        :return: Dictionary of team numbers and names
+        :exe: {1: 'School Name A', 2: 'School Name B', ...}
+        """
+
+        return self._teams
+
+    @property
+    def events(self) -> list[str]:
+        """
+        :return: List of event names
+        :exe: ['Event 1', 'Event 2', ...]
+        """
+
+        return self._events
+
+    @property
+    def trial_events(self) -> list[str]:
+        """
+        :return: List of trial event names
+        :exe: ['Event 1', 'Event 2', ...]
+        """
+
+        return self._trial_events
+
+    @property
+    def full_scores(self) -> dict[int, list[int]]:
+        """
+        :return: Dictionary of team numbers and full array of scores
+        :exe: {1: [1, 2, 3], 2: [1, 2, 3], ...}
+        """
+        return self._full_scores
+
+    @property
+    def score_sum(self) -> dict[int, int]:
+        """
+        :return: Dictionary of team numbers and total score
+        :exe: {1: 100, 2: 200, ...}
+        """
+
+        return self._score_sum
+
+    @property
+    def averages(self) -> dict[int, float]:
+        """
+        :return: Dictionary of team numbers and average score
+        :exe: {1: 100.0, 2: 200.0, ...}
+        """
+
+        return self._averages
+
+    @property
+    def raw_placements(self) -> list[dict[str, str | int]]:
+        return self._raw_placements
